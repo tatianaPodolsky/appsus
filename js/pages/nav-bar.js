6fa0caac86391575bb91d734934daa25dde8713b
Tatiana Podolsky <podolskaya.tatiana@gmail.com>
2019-02-27 10:27:43 +0200

    !

---
+++ js/pages/nav-bar.js
@@ -0,0 +1,13 @@
+export default {
+    template: `
+    <section class="nav-bar">
+        <router-link to="/" exact>Home</router-link> 
+        <router-link to="/mail-app">Mail App</router-link>
+        <router-link to="/keep-app">Keep App</router-link>
+        <router-link to="/about">About</router-link> 
+        <router-link to="/contacts">Contacts</router-link> 
+
+    </section>
+
+    `
+}
---
+++ js/routes.js
@@ -0,0 +1,8 @@
+const routes = [
+    { path: '/', component: homeCmp },
+    { path: '/about', component: aboutCmp},
+    { path: '/mail-app', component: mailApp },
+    { path: '/keep-app', component: keepApp },
+    { path: '/contacts', component: contacts },
+]
+export default routes;
