df08f179784f58e0164d5c7d020d02622069091e
Daniel Borisov <dannyboris1993@gmail.com>
2019-02-27 16:54:04 +0200

    mail init and imports

--- js/services/mail-service.js
+++ js/services/mail-service.js
@@ -1,10 +1,18 @@
-var gMails = [{name:'Danny',subject:'Cats','The big cat'},
+export default {
+    getMails
+}
 
-{name:'Danny',subject:'Cats','The big cat'},
-{name:'Danny',subject:'Cats','The big cat'}];
-
-
+var gMails = [
+    {name:'Danny',subject:'Cats',title:'The big cat',body:'lorem ipsum'},
+    {name:'Tai',subject:'Dogs',title:'The small puppy',body:'lorem ipsum'},
+    {name:'Mor',subject:'Bird',title:'The tiny bird',body:'lorem ipsum'}
+];
 
 function getMails(){
-    if()
+    return  Promise.resolve(gMails)
 }
+
+
+
+
+
