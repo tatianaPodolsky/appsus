b2b0a783f207ffc4512e2f38bbd317ebda8aa3f7
Daniel Borisov <dannyboris1993@gmail.com>
2019-02-28 09:21:40 +0200

    good morning viatnam

--- js/cmps/mail/mail-list-cmp.js
+++ js/cmps/mail/mail-list-cmp.js
@@ -1,16 +1,31 @@
 import mailPreview from './mail-preview-cmp.js'
+import mailDetails from './mail-details-cmp.js'
+
 export default{
     template:`
     <section>
         <h1>Email list</h1>
-        <mail-preview v-for="mail in mails" :key="mail.id" 
+        <mail-preview @click="selectMail(mail)" v-for="mail in mails" :key="mail.id" 
         :mail="mail"></mail-preview>
-        <
+        <mail-details v-if="selectedMail"></mail-details>
+        
     </section>
     
     `,
     props:['mails'],
     components:{
         mailPreview,
+    },
+    data(){
+        return{
+            selectedMail:null
+        }
+    },
+    methods:{
+        selectMail(mail){
+            this.selectedMail = mail
+            console.log(selectedMail)
+        }
     }
+    
 }
--- js/cmps/mail/mail-preview-cmp.js
+++ js/cmps/mail/mail-preview-cmp.js
@@ -1,27 +1,22 @@
-import mailDetails from './mail-details-cmp.js'
 import {eventBus} from '../../event-bus.js'
 export default{
     template:`
-    <section class="mail-preview">
-        <div :class="{read:mail.isRead}" class="flex space-around">
+    <section  :class="{read:mail.isRead}" class="mail-preview" flex space-around>
             <input v-model="mail.isRead" @change="checked(mail)"  type="checkbox">
             <p>From: {{mail.name}} </p>
             <p>Subject: {{mail.subject}}</p>
             <p>content: {{mail.body}}</p>
             <p>{{mail.time}}</p>
-        <mail-details v-if="selectedMail"></mail-details>
 
-        </div>
     </section>
     
     `,
     data(){
         return{
-            selectedMail:null
         }
     },
     components:{
-        mailDetails,
+        
         
     },
 
